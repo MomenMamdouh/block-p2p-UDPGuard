c3e79cfdc02af604772bc7d0dbbd9141caf6477e
Mo'men Mamdouh <Momen.Mamdouh@gmail.com>
2026-06-11 21:06:22 +0300

    Update block-p2p.sh

--- block-p2p.sh
+++ block-p2p.sh
@@ -14,7 +14,7 @@ fi
 # ------------------------------------------------------------------------------
 IPSET_NAME="vless_torrent_block"
 XRAY_LOG="/etc/x-ui/access.log"
-BAN_DURATION=18000   # 5 Hours
+BAN_DURATION=1800   # 30 Minutes 
 MAX_ENTRIES=200000
 
 # ------------------------------------------------------------------------------
